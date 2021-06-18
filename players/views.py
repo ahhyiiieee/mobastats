e9bc909b273a2f976b9f97279d7d51f438bed594
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-19 01:15:07 +0800

    Add function latestGame to views

--- players/views.py
+++ players/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
+from .forms import LatestGameForm
 
 # Create your views here.
 def latestGame(request):
-    return render(request, 'players/latest_game.html')
+    context = {}
+    context['form'] = LatestGameForm()
+    return render(request, 'players/latest_game.html', context)
+
+
+def last_5_games(request):
+    last_5_games = Game.object.all()
+
+    return render(request, 'home.html', {'last_5_games':last_5_games})
