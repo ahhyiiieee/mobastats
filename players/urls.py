f5dcfa6fa292dd3aaf17686abc651dbae1cf1f73
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-20 04:16:56 +0800

    Update urlpattern

--- players/urls.py
+++ players/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('latest_game/', views.latestGame, name='latest_game'),
+    path('latest_game_form/', views.latest_game_form, name='latest_game_form'),
 ]
