c0a4512ad0ea1774c793c66b0719e8d2e3835df7
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-20 04:16:34 +0800

    Edit function name latest_game_form

--- players/views.py
+++ players/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from .models import *
 from .forms import LatestGameForm
 
+
 # Create your views here.
-def latestGame(request):
+def latest_game_form(request):
     context = {}
     context['form'] = LatestGameForm()
-    return render(request, 'players/latest_game.html', context)
+    return render(request, 'players/latest_game_form.html', context)
