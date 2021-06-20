3dcd5b17665df5f333752005db21dbb5f8cf4d81
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-20 14:09:20 +0800

    Add player_top_5_heroes

--- config/views.py
+++ config/views.py
@@ -5,7 +5,16 @@ from players import models as players_models
 
 @login_required
 def home(request):
-    last_5_games = players_models.Game.objects.all().filter(player=request.user).order_by('-date')[:5]
+    game = players_models.Game.objects.all()
+
+    """
+    num_games_won = game.filter(player=request.user,result=1).count()
+    num_games_lost = game.filter(player=request.user,result=2).count()
+    num_games = num_games_won + num_games_lost
+    win_rate = num_games_won / num_games * 100
+    """
+
+    last_5_games = game.filter(player=request.user).order_by('-date')[:5]
     player_top_5_heroes = players_models.PlayerHero.objects.all().filter(player=request.user).order_by('-winrate')[:5]
     #top_5_heroes =
 
