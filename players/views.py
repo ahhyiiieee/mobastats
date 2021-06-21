2e3ab31eb2c84bbc89250ffd9d7cbb60c117eca0
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-22 02:16:16 +0800

    Add try and except block for ZeroDivisionError

--- players/views.py
+++ players/views.py
@@ -12,49 +12,47 @@ def latest_game_form(request):
     if request.method == 'POST':
         form = LatestGameForm(request.POST)
         if form.is_valid():
-            game = form.save(commit=False)
-            game.player = request.user
-            game.save()
-
-            #print(game.hero)
-            #print(game.player)
-            
-            num_games_won = Game.objects.filter(
-                player=game.player,
-                result=Game.WIN,
-                hero=game.hero,
-            ).count()
-            num_games_lost = Game.objects.filter(
-                player=game.player,
-                result=Game.LOST,
-                hero=game.hero,
-            ).count()
-            num_games = num_games_won + num_games_lost
-            winrate = (num_games_won / num_games) * 100
-            PlayerHero.objects.update_or_create(
-                player=game.player,
-                hero=game.hero,
-                defaults={
-                    'winrate': winrate,
-                    'num_games': num_games,
-                }
-            )
-            global_num_games_won = Game.objects.filter(
-                result=Game.WIN,
-                hero=game.hero,
-            ).count()
-            global_num_games_lost = Game.objects.filter(
-                result=Game.LOST,
-                hero=game.hero,
-            ).count()
-            global_num_games = global_num_games_won + global_num_games_lost
-            global_winrate = (global_num_games_won / global_num_games) * 100
-            hero = game.hero
-            hero.num_games = global_num_games
-            hero.winrate = global_winrate
-            hero.save()
-
-            return redirect('home')
+            try:
+                game = form.save(commit=False)
+                game.player = request.user
+                game.save()    
+                num_games_won = Game.objects.filter(
+                    player=game.player,
+                    result=Game.WIN,
+                    hero=game.hero,
+                ).count()
+                num_games_lost = Game.objects.filter(
+                    player=game.player,
+                    result=Game.LOST,
+                    hero=game.hero,
+                ).count()
+                num_games = num_games_won + num_games_lost
+                winrate = (num_games_won / num_games) * 100
+                PlayerHero.objects.update_or_create(
+                    player=game.player,
+                    hero=game.hero,
+                    defaults={
+                        'winrate': winrate,
+                        'num_games': num_games,
+                    }
+                )
+                global_num_games_won = Game.objects.filter(
+                    result=Game.WIN,
+                    hero=game.hero,
+                ).count()
+                global_num_games_lost = Game.objects.filter(
+                    result=Game.LOST,
+                    hero=game.hero,
+                ).count()
+                global_num_games = global_num_games_won + global_num_games_lost
+                global_winrate = (global_num_games_won / global_num_games) * 100
+                hero = game.hero
+                hero.num_games = global_num_games
+                hero.winrate = global_winrate
+                hero.save()
+                return redirect('home')
+            except ZeroDivisionError:
+                return redirect('home')
     else:
         form = LatestGameForm(initial={'date': timezone.now()})
         context = {
