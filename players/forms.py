bce187e42bf9d02985dea31ae332a616ca404fe2
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-22 01:45:48 +0800

    Used ModelForm on LatestGameForm

--- players/forms.py
+++ players/forms.py
@@ -1,18 +1,21 @@
-from django import forms
+from django.forms import ModelForm
 from django.utils import timezone
 from django.forms.fields import DateTimeField
+from django import forms
 
 from .models import Game
 
 
 # creating a form for recording games
-# result choices
-
-class LatestGameForm(forms.Form):
-    hero = forms.CharField()
-    result = forms.CharField(label='Result', widget=forms.Select(choices=Game.RESULT_TYPES))
-    kills = forms.IntegerField()
-    deaths = forms.IntegerField()
-    assists = forms.IntegerField()
-    date = forms.DateTimeField(initial=timezone.now())
-    duration = forms.TimeField()
+class LatestGameForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = [
+            'hero',
+            'result',
+            'kills',
+            'deaths',
+            'assists',
+            'date',
+            'duration'
+        ]
