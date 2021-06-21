cb97799595c58d0e1e7f6e173fe8b4321a2f3400
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-22 01:43:28 +0800

    Add recording formulas and conditions

--- players/views.py
+++ players/views.py
@@ -1,10 +1,63 @@
-from django.shortcuts import render
-from .models import *
+from django.http import request
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .models import PlayerHero, Game, Hero
 from .forms import LatestGameForm
+
 
 
 # Create your views here.
 def latest_game_form(request):
-    context = {}
-    context['form'] = LatestGameForm()
-    return render(request, 'players/latest_game_form.html', context)
+    if request.method == 'POST':
+        form = LatestGameForm(request.POST)
+        if form.is_valid():
+            game = form.save(commit=False)
+            game.player = request.user
+            game.save()
+
+            #print(game.hero)
+            #print(game.player)
+            
+            num_games_won = Game.objects.filter(
+                player=game.player,
+                result=Game.WIN,
+                hero=game.hero,
+            ).count()
+            num_games_lost = Game.objects.filter(
+                player=game.player,
+                result=Game.LOST,
+                hero=game.hero,
+            ).count()
+            num_games = num_games_won + num_games_lost
+            winrate = (num_games_won / num_games) * 100
+            PlayerHero.objects.update_or_create(
+                player=game.player,
+                hero=game.hero,
+                defaults={
+                    'winrate': winrate,
+                    'num_games': num_games,
+                }
+            )
+            global_num_games_won = Game.objects.filter(
+                result=Game.WIN,
+                hero=game.hero,
+            ).count()
+            global_num_games_lost = Game.objects.filter(
+                result=Game.LOST,
+                hero=game.hero,
+            ).count()
+            global_num_games = global_num_games_won + global_num_games_lost
+            global_winrate = (global_num_games_won / global_num_games) * 100
+            hero = game.hero
+            hero.num_games = global_num_games
+            hero.winrate = global_winrate
+            hero.save()
+
+            return redirect('home')
+    else:
+        form = LatestGameForm(initial={'date': timezone.now()})
+        context = {
+            'form':form,
+        }
+        return render(request, 'players/latest_game_form.html', context)
