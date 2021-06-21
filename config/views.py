0cf486412deb4c001ff63a4aa1f5fb2027bc4db6
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-22 01:47:36 +0800

    Add formulas for populating Top Heroes global and individual

--- config/views.py
+++ config/views.py
@@ -6,21 +6,15 @@ from players import models as players_models
 @login_required
 def home(request):
     game = players_models.Game.objects.all()
-
-    """
-    num_games_won = game.filter(player=request.user,result=1).count()
-    num_games_lost = game.filter(player=request.user,result=2).count()
-    num_games = num_games_won + num_games_lost
-    win_rate = num_games_won / num_games * 100
-    """
-
+    hero = players_models.Hero.objects.all()
     last_5_games = game.filter(player=request.user).order_by('-date')[:5]
     player_top_5_heroes = players_models.PlayerHero.objects.all().filter(player=request.user).order_by('-winrate')[:5]
-    #top_5_heroes =
+    top_5_heroes = hero.filter(num_games__gte=1).order_by('-winrate')[:5]
 
     context = {
         'player': request.user,
         'last_5_games': last_5_games,
         'player_top_5_heroes': player_top_5_heroes,
+        'top_5_heroes': top_5_heroes,
     }
     return render(request, 'home.html', context)
