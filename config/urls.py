492383efcb3b2a41e7acb1d5872a732436585cfc
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-06-22 01:44:38 +0800

    Add name to home url

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/',auth_views.LogoutView.as_view() , name='logout'),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('', include('players.urls')),
 ]
