04f253d5ea5bc5ea445121fd4f5606f8f46ac7d2
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-05 20:05:09 +0800

    Breakdown Game models to Game and GamePlayer Model

--- players/models.py
+++ players/models.py
@@ -14,6 +14,26 @@ class Hero(models.Model):
     def __str__(self):
         return self.name
 
+    @classmethod
+    def update_stats(cls, hero):
+        global_num_games_won = GamePlayer.objects.filter(
+            result=GamePlayer.WIN,
+            hero=hero,
+        ).count()
+        global_num_games_lost = GamePlayer.objects.filter(
+            result=GamePlayer.LOST,
+            hero=hero,
+        ).count()
+        global_num_games = global_num_games_won + global_num_games_lost
+        try:
+            global_winrate = (global_num_games_won / global_num_games) * 100
+        except ZeroDivisionError:
+            global_winrate = 0
+        hero = hero
+        hero.num_games = global_num_games
+        hero.winrate = global_winrate
+        hero.save()
+
 
 class PlayerHero(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
@@ -24,8 +44,42 @@ class PlayerHero(models.Model):
     def __str__(self):
         return f'{self.player} - {self.hero}'
 
+    @classmethod
+    def update_stats(cls, player, hero):
+        num_games_won = GamePlayer.objects.filter(
+            player=player,
+            result=GamePlayer.WIN,  
+            hero=hero,
+        ).count()
+        num_games_lost = GamePlayer.objects.filter(
+            player=player,
+            result=GamePlayer.LOST,
+            hero=hero,
+        ).count()
+        num_games = num_games_won + num_games_lost
+        try:
+            winrate = (num_games_won / num_games) * 100
+        except ZeroDivisionError:
+            winrate = 0
+        PlayerHero.objects.update_or_create(
+            player=player,
+            hero=hero,
+            defaults={
+                'winrate': winrate,
+                'num_games': num_games,
+            }
+        )
+
 
 class Game(models.Model):
+    date = models.DateTimeField()
+    duration = models.DurationField()
+
+    def __str__(self):
+        return f'{self.date} - {self.duration}'
+
+
+class GamePlayer(models.Model):
     WIN = 1
     LOST = 2
     INVALID = 3
@@ -36,14 +90,34 @@ class Game(models.Model):
         (INVALID, 'Invalid'),
     ]
 
+    BLUE = 1
+    RED = 2
+
+    TEAM_SIDE = [
+        (BLUE,'Blue Side'),
+        (RED,'Red Side'),
+    ]
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     hero = models.ForeignKey(Hero, on_delete=models.CASCADE)
     result = models.PositiveSmallIntegerField(choices=RESULT_TYPES)
+    team = models.PositiveSmallIntegerField(choices=TEAM_SIDE)
     kills = models.IntegerField()
     deaths = models.IntegerField()
     assists = models.IntegerField()
-    date = models.DateTimeField()
-    duration = models.DurationField()
-
+    
     def __str__(self):
-        return f'{self.player} - {self.hero} ({self.date})'
+        return f'{self.player} - {self.hero} - {self.team}'
+        
+# A game can involve 10 players, and they don't have to enter their own game if someone else entered it.
+
+'''
+1. Separate entry for all players: Hero, K/D/A.
+2. 2 groups of 5, winning side and losing side
+
+Break down the Game model into two models, 
+Game (containing date and duration) and 
+GamePlayer (foreign keys to Game and Player, and hero/result/kda). 
+The goal is to be able to enter one game that ties to multiple players.
+'''
