ec9a2644f9e285c540838d765384327f2bebbe4c
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-05 20:31:27 +0800

    Register GamePlayer to admin site

--- players/admin.py
+++ players/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Player, Hero, PlayerHero, Game
+from .models import Player, Hero, PlayerHero, Game, GamePlayer
 
 admin.site.register(Player)
 admin.site.register(Hero)
 admin.site.register(PlayerHero)
 admin.site.register(Game)
+admin.site.register(GamePlayer)
