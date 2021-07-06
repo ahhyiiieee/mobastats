5729545b1b21b2e352282e8fdde4be29dc35121a
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-06 17:33:31 +0800

    Add winrate__gte1 for filtering top heroes

--- config/views.py
+++ config/views.py
@@ -9,8 +9,11 @@ def home(request):
     hero = players_models.Hero.objects.all()
     game_players = players_models.GamePlayer.objects.all()
     last_5_games = game_players.filter(player=request.user).order_by('-game')[:5]
-    player_top_5_heroes = players_models.PlayerHero.objects.all().filter(player=request.user).order_by('-winrate', '-num_games', 'hero__name')[:5]
-    top_5_heroes = hero.filter(num_games__gte=1).order_by('-winrate', '-num_games', 'name')[:5]
+    
+    #Cannot resolve keyword 'gameplayer' into field next line
+    #last_5_games = game_players.filter(gameplayer__player=request.user).order_by('-game')[:5]
+    player_top_5_heroes = players_models.PlayerHero.objects.all().filter(player=request.user, num_games__gte=1, winrate__gte=1).order_by('-winrate', '-num_games', 'hero__name')[:5]
+    top_5_heroes = hero.filter(num_games__gte=1, winrate__gte=1).order_by('-winrate', '-num_games', 'name')[:5]
 
     context = {
         'player': request.user,
