7fbca8b69a8b2e50d5031fe6a52d7d59cd808d87
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-06 15:25:57 +0800

    Add url path for game and player details

--- players/urls.py
+++ players/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('latest_game_form/', views.latest_game_form, name='latest_game_form'),
+    path('game_details/', views.game_details, name='game_details'),
+    path('player_details/', views.player_details, name='player_details'),
 ]
