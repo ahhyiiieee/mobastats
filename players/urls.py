5763fca5e13f00a840103ee378ad7b267c084eaf
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-07 21:10:10 +0800

    Add <int:pk> for game_details and player_details url path

--- players/urls.py
+++ players/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('latest_game_form/', views.latest_game_form, name='latest_game_form'),
-    path('game_details/', views.game_details, name='game_details'),
-    path('player_details/', views.player_details, name='player_details'),
+    path('game_details/<int:pk>/', views.game_details, name='game_details'),
+    path('player_details/<int:pk>/', views.player_details, name='player_details'),
 ]
