37674823080fd8b353405b0758273374ee24e6a3
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-09 12:45:35 +0800

    Converted GamePlayerForm ModelForm to Form

--- players/forms.py
+++ players/forms.py
@@ -7,20 +7,6 @@ from .models import GamePlayer, Game
 
 
 # creating a form for recording games
-class GamePlayerForm(forms.ModelForm):
-    class Meta:
-        model = GamePlayer
-        fields = [
-            'team',
-            'result',
-            'player',
-            'hero',
-            'kills',
-            'deaths',
-            'assists',
-        ]
-
-
 class GameForm(forms.ModelForm):
     class Meta:
         model = Game
@@ -29,6 +15,34 @@ class GameForm(forms.ModelForm):
             'duration',
         ]
         
+
+class GamePlayerForm(forms.Form):
+    WIN = 1
+    LOST = 2
+    INVALID = 3
+
+    RESULT_TYPES = [
+        (WIN, 'Win'),
+        (LOST, 'Lost'),
+        (INVALID, 'Invalid'),
+    ]
+
+    BLUE = 1
+    RED = 2
+
+    TEAM_SIDE = [
+        (BLUE,'Blue'),
+        (RED,'Red'),
+    ]
+
+    team = forms.CharField(widget=forms.Select(choices=RESULT_TYPES))
+    result = forms.CharField(widget=forms.Select(choices=TEAM_SIDE))
+    player = forms.CharField()
+    hero = forms.CharField()
+    kills = forms.IntegerField()
+    deaths = forms.IntegerField()
+    assists = forms.IntegerField()
+
 #make another form for game
 #then render the 2 forms together
 #1 Game form, 10 GamePlayer forms later task
