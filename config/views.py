bb2a0070fb50131c38b8de158ae32b5ab1021072
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-09 12:42:08 +0800

    Removed game queryset and request.user in context, applied -game__date in filter

--- config/views.py
+++ config/views.py
@@ -5,7 +5,6 @@ from players import models as players_models
 
 @login_required
 def home(request):
-    game = players_models.Game.objects.all()
     hero = players_models.Hero.objects.all()
     game_players = players_models.GamePlayer.objects.all()
     last_5_games = game_players.filter(player=request.user).order_by('-game__date')[:5]
@@ -16,7 +15,6 @@ def home(request):
     top_5_heroes = hero.filter(num_games__gte=1).order_by('-winrate', '-num_games', 'name')[:5]
 
     context = {
-        'player': request.user,
         'last_5_games': last_5_games,
         'player_top_5_heroes': player_top_5_heroes,
         'top_5_heroes': top_5_heroes,
