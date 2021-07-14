c28bd2b0dacbe356e4a8eb3b833c98e1101be940
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-14 21:20:03 +0800

    Fixed logic flow of latest_game_form function

--- players/views.py
+++ players/views.py
@@ -3,37 +3,49 @@ from django.forms.models import ModelMultipleChoiceField
 from django.http import request
 from django.shortcuts import redirect, render
 from django.utils import timezone
-from django.forms import fields
+from django.forms import fields, modelformset_factory
 
 from .models import Player, PlayerHero, Game, Hero, GamePlayer
-from .forms import GameForm, GamePlayerFormSet
+from .forms import GameForm, GamePlayerForm
 
 # 1. Make sure to place your exception handler as close to the source of the exception as possible
 # 2. In general, don't write too long functions. See if you can extract distinct pieces of logic into their own functions 
 
 # Create your views here.
 def latest_game_form(request):
+    game_form = GameForm(initial={'date': timezone.now()})
+    GamePlayerFormSet = modelformset_factory(GamePlayer, form=GamePlayerForm, extra=5)
+    winner_formset = GamePlayerFormSet(queryset=GamePlayer.objects.none(), initial=[{'game': game_form,}], prefix='winner')  
+    loser_formset = GamePlayerFormSet(queryset=GamePlayer.objects.none(), initial=[{'game': game_form,}], prefix='loser') 
     if request.method == 'POST':
         game_form = GameForm(request.POST)
-        winner_formset = GamePlayerFormSet(request.POST)
-        if game_form.is_valid() and winner_formset.is_valid():
+        winner_formset = GamePlayerFormSet(request.POST, prefix='winner')
+        loser_formset = GamePlayerFormSet(request.POST, prefix='loser')
+        if game_form.is_valid():
             game = game_form.save()
+        if winner_formset.is_valid():
             instances = winner_formset.save(commit=False)
             for instance in instances:
                 instance.game = game
                 instance.save()
                 PlayerHero.update_stats(instance.player, instance.hero)
                 Hero.update_stats(instance.hero)
-                return redirect('home')
-        else:
-            print(game_form.errors, winner_formset.errors)
+        if loser_formset.is_valid():
+            instances = loser_formset.save(commit=False)
+            for instance in instances:
+                instance.game = game
+                instance.save()
+                PlayerHero.update_stats(instance.player, instance.hero)
+                Hero.update_stats(instance.hero)   
+        
+        return redirect('home')
+    else:
+        print(game_form.errors, winner_formset.errors)
                         
-    game_form = GameForm(initial={'date': timezone.now()})
-    winner_formset = GamePlayerFormSet(queryset=GamePlayer.objects.none(),)            
-    
     context = {
         'game_form': game_form,
         'winner_formset': winner_formset,
+        'loser_formset': loser_formset,
         }
         
     return render(request, 'players/latest_game_form.html', context)
