cf9ec2bb63253839572b0b3cd4e36a3b583ea45a
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-16 17:13:29 +0800

    Added initial filter for PlayerHero model. Break long line codes.

--- config/views.py
+++ config/views.py
@@ -5,13 +5,14 @@ from players import models as players_models
 
 @login_required
 def home(request):
-    hero = players_models.Hero.objects.all()
     game_players = players_models.GamePlayer.objects.all()
-    last_5_games = game_players.filter(player=request.user).order_by('-game__date')[:5]
-    
-    #Cannot resolve keyword 'gameplayer' into field 
-    #last_5_games = game_players.filter(gameplayer__player=request.user).order_by('-game')[:5]
-    player_top_5_heroes = players_models.PlayerHero.objects.all().filter(player=request.user, num_games__gte=1).order_by('-winrate', '-num_games', 'hero__name')[:5]
+    player_heroes = players_models.PlayerHero.objects.all()
+    hero = players_models.Hero.objects.all()
+
+    last_5_games = game_players.filter(
+        player=request.user).order_by('-game__date')[:5]
+    player_top_5_heroes = player_heroes.filter(
+        player=request.user, num_games__gte=1).order_by('-winrate', '-num_games', 'hero__name')[:5]
     top_5_heroes = hero.filter(num_games__gte=1).order_by('-winrate', '-num_games', 'name')[:5]
 
     context = {
@@ -19,4 +20,5 @@ def home(request):
         'player_top_5_heroes': player_top_5_heroes,
         'top_5_heroes': top_5_heroes,
     }
+
     return render(request, 'home.html', context)
