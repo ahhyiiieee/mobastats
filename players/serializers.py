f60a5e93f54f8ee646e27d60cbbdc3fc0c559ff7
Josef Hermosa <josefmariel.hermosa@gmail.com>
2021-07-30 22:15:40 +0800

    Add SerializerMethodField for player top 5 heroes

--- players/serializers.py
+++ players/serializers.py
@@ -10,6 +10,7 @@ class HeroSerializer(serializers.ModelSerializer):
 
 class PlayerHeroSerializer(serializers.ModelSerializer):
     # hero_display = HeroSerializer(source='hero', read_only=True)
+    # hero_name = serializers.CharField(source='hero')
 
     class Meta:
         model = PlayerHero
@@ -17,17 +18,25 @@ class PlayerHeroSerializer(serializers.ModelSerializer):
 
 
 class PlayerSerializer(serializers.ModelSerializer):
+    # player_heroes = PlayerHeroSerializer('player_heroes', many=True, read_only=True)
+    top_5_heroes = serializers.SerializerMethodField()
+
     class Meta:
         model = Player
         fields = [
             'id',
+            'username',
             'display_name',
+            'top_5_heroes',
         ]
+
+    def get_top_5_heroes(self, obj):
+        top_5_heroes = obj.player_heroes.order_by('-winrate', '-num_games', 'hero__name')[:5]
+        return PlayerHeroSerializer(top_5_heroes, many=True).data
 
 
 class GamePlayerSerializer(serializers.ModelSerializer):
-    player_data = PlayerSerializer(source='player', read_only=True)
-
+    # player_data = PlayerSerializer(source='player', read_only=True)
     class Meta:
         model = GamePlayer
         fields = '__all__'
